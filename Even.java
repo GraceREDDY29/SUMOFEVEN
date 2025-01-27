32aac8eb20d857b59c2a36a73a4960c5ae148ab7
GraceREDDY29 <gundampalligrace@gmail.com>
2025-01-27 23:49:11 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,15 @@
+public class Even {
+    public static void main (String[] args){
+        int result=0;
+        for(int i =0;i<=100;i=i+2){
+            result=result+i;
+            System.out.println(result);
+        }
+    }
+    
+}
+
+
+
+    
+
